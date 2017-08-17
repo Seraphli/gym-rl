c88ad03c15c3ec07100ca733075e9c217b348c43
seraphli <seraphlivery@gmail.com>
2017-08-17 18:54:32 +0800

    add visualize

--- util/tf_common.py
+++ util/tf_common.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow import Tensor
 
 
 def huber_loss(x, delta=1.0):
@@ -20,3 +21,46 @@ def minimize_and_clip(optimizer, objective, var_list, clip_val=10):
         if grad is not None:
             gradients[i] = (tf.clip_by_norm(grad, clip_val), var)
     return optimizer.apply_gradients(gradients)
+
+
+def vis_conv_layer(v, shape):
+    """Visualize convolution layer
+    
+    Args:
+        v (Tensor): output of the layer,
+            with shape (batch, height, width, channels)
+        shape (tuple): a tuple of height, width,
+            channels, number of images in one line, number of lines
+
+    Returns:
+        Tensor: combined image of the output
+
+    """
+    h, w, c, cy, cx = shape
+    v = tf.slice(v, (0, 0, 0, 0), (1, -1, -1, -1))
+    v = tf.reshape(v, (h, w, c))
+    w += 4
+    h += 4
+    v = tf.image.resize_image_with_crop_or_pad(v, h, w)
+    v = tf.reshape(v, (1, h, w, cy, cx))
+    v = tf.transpose(v, (0, 3, 1, 4, 2))
+    v = tf.reshape(v, (1, cy * h, cx * w, 1))
+    return v
+
+
+def vis_fc_layer(v, shape):
+    """Visualize full-connected layer
+    
+    Args:
+        v (Tensor): output of the layer,
+            with shape (batch, depth)
+        shape (tuple): a tuple of height, width
+
+    Returns:
+        Tensor: one image of the output
+
+    """
+    h, w = shape
+    v = tf.slice(v, (0, 0), (1, -1))
+    v = tf.reshape(v, (1, h, w, 1))
+    return v
